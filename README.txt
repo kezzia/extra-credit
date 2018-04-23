d95da7409b9f9bf2e843ec7fa5f665a8e076ca06
KezziaCarter <33240092+KezziaCarter@users.noreply.github.com>
2018-04-23 16:54:59 -0400

    added output screenshot and readme

---
+++ README.txt
@@ -0,0 +1,5 @@
+Kezzia Carter - @02746973
+How to run this code:
+1. Go to https://repl.it/repls/OrangeAffectionateMicroprogramming
+2. Copy & paste code into provided field
+3. Hit run
--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 '''
-1) Mean
-2) Variance
-3) Minimum and Maximum
-4) Median
-5) Mode
+Kezzia Carter - @02746973
+How to run this code:
+1. Go to https://repl.it/repls/OrangeAffectionateMicroprogramming
+2. Copy & paste code into provided field
+3. Hit run
 '''
 
 def Mean(N):
